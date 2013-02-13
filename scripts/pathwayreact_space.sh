955c6c9ff7a515a555c23843c5886f49a4f534eb
Dana Klassen <klassen.dana@gmail.com>
2013-02-13 15:04:49 +0000

    added sabio datasource

--- scripts/pathwayreact_space.sh
+++ scripts/pathwayreact_space.sh
@@ -7,48 +7,40 @@
 set -e
 umask 0022
 
-SPACE_NAME="pathwayreact_space"			# Name of the endpoint being created
-data_dir=/opt/data/${SPACE_NAME}		# Where the data will be placed
-db_dir=${data_dir}/virtuoso/			# Where the virtuoso db will be constructucted
-
 root_dir=$(dirname $(dirname "$(pwd)/.."))
-scripts="${root_dir}/dataspaces/${SPACE_NAME}"
-logfile="${data_dir}/${SPACE_NAME}_$(date +"%Y-%m-%d").log"
-
 cd $root_dir
 source ./lib/functions.sh
 source ./lib/common.sh
 
-# Check virtuoso can be found
-check_virtuoso_install
-if [  $? -ne 0 ];
-	then
-	exit 1
-fi
+SPACE_NAME="pathwayreact_space"			# Name of the endpoint being created
+data_dir=/opt/data/${SPACE_NAME}		# Where the data will be placed
+db_dir=${data_dir}/virtuoso/			# Where the virtuoso db will be constructucted
 
-# Create the data directories
-setup_data_dir
-
-touch $logfile
-echo "INFO: Logging to $logfile"
-
-# Directory where we are going to put everything
-if [ ! -d "$scripts" ];then
-	mkdir -p $scripts
-fi
+scripts="${root_dir}/dataspaces/${SPACE_NAME}"
+logfile="${data_dir}/${SPACE_NAME}_$(date +"%Y-%m-%d").log"
 
 # # List of source scripts to download and run format: [ name script_url files_to_process ]
 sources[0]="biomodels https://raw.github.com/dklassen/bio2rdf-scripts/master/biomodels/biomodels.php"
 sources[1]="biopax https://raw.github.com/bio2rdf/bio2rdf-scripts/master/biopax/biopax.php"
+sources[2]="sabiork https://raw.github.com/dklassen/bio2rdf-scripts/master/sabiork/sabiork.php"
 
-if [ ! -d "${root_dir}/dataspaces/arc2" ]; then
-	cd ${root_dir}/dataspaces/
-	wget https://github.com/semsol/arc2/tarball/master -O arc.tar.gz
-	tar -xvf arc.tar.gz
-	mv semsol-arc2-*/ arc2/
-fi
+function setup_dataspace(){
+	check_virtuoso_install
+	check_dependencies
+	setup_data_dir
+	
+	touch $logfile
+	echo "INFO: Logging to $logfile"
+
+	# Directory where we are going to put everything
+	if [ ! -d "$scripts" ];then
+		mkdir -p $scripts
+	fi
+}
+
 
 # Download kegg data from bio2rdf since requires paid subscription
+setup_dataspace
 download_kegg
 generate_data
 build_database
